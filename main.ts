48e92ed44e14eea3f9612a34aa559cbf92c211c0
magggieyang171 <116053282+magggieyang171@users.noreply.github.com>
2022-12-31 17:04:43 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("G F G A - F E D ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("A F E F D G E F ", 120)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . . . #
+        `)
+})
